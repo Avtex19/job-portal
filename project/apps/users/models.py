b8f3ce0a2d47d38fbc5bf21ef94f7b1b40e5b356
Avtex19 <atukaberadze19@gmail.com>
2026-01-27 13:32:50 +0400

    feat(auth): implement custom User model with email login and roles
    - Replace default Django User with custom model in `apps.users`.
    - Remove `username` field and set `email` as the unique identifier.
    - Add `role` field (Admin, Candidate, Employer) for RBAC.
    - Implement `CustomUserManager` to handle user creation without usernames.
    - Configure `AUTH_USER_MODEL` in settings to point to `users.User`.

--- project/apps/users/models.py
+++ project/apps/users/models.py
@@ -1,3 +1,28 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from apps.users.managers import UserManager
+
+
 # Create your models here.
+class User(AbstractUser):
+    class Roles(models.TextChoices):
+        ADMIN = 'ADMIN', 'Admin'
+        CANDIDATE = 'CANDIDATE', 'Candidate'
+        EMPLOYER = 'EMPLOYER', 'Employer'
+
+
+    username = None
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=50, blank=False)
+    last_name = models.CharField(max_length=50, blank=False)
+    role = models.CharField(
+        max_length=20,
+        choices=Roles.choices,
+        default=Roles.CANDIDATE,
+    blank=False,
+    null=False,
+    )
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+    objects = UserManager()
