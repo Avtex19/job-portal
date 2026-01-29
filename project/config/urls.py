a05ba1fcece7dd6852a71be0523346b40094d1ad
Avtex19 <atukaberadze19@gmail.com>
2026-01-29 16:55:21 +0400

    feat(auth): add SimpleJWT and update dependency lockfile

--- project/config/urls.py
+++ project/config/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.http import HttpResponse
 
 
@@ -26,5 +26,6 @@ def hello(_request):
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', hello, name='hello'),
+    path('', include('apps.users.urls')),
 
 ]
