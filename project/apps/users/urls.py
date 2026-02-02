fc0098f94ba4956434a8da79528aeb7335e600cf
Avtex19 <atukaberadze19@gmail.com>
2026-02-02 13:04:21 +0400

    feat(users): add user registration API endpoint

--- project/apps/users/urls.py
+++ project/apps/users/urls.py
@@ -5,7 +5,12 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
+from apps.users.views import RegisterAPIView
+
 urlpatterns = [
+    path('auth/register/', RegisterAPIView.as_view(), name='user-register'),
+
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
 ]
