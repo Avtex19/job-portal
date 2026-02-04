41c2d86a325f8610cb64f9a92c4313f636c1cd36
Avtex19 <atukaberadze19@gmail.com>
2026-02-04 14:28:32 +0400

    fix: update import

--- project/apps/users/models.py
+++ project/apps/users/models.py
@@ -1,8 +1,8 @@
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 from apps.users.managers import UserManager
-from config import settings
 
 
 # Create your models here.
