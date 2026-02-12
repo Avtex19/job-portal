4c58900a9e04430e2de8ac35a1ce5ca9aa6560f3
Avtex19 <atukaberadze19@gmail.com>
2026-02-12 21:08:13 +0400

    feat: create  view for user login and map url

--- project/apps/users/urls.py
+++ project/apps/users/urls.py
@@ -5,10 +5,11 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
-from apps.users.views import RegisterAPIView, VerifyEmailAPIView
+from apps.users.views import RegisterAPIView, VerifyEmailAPIView, LoginAPIView
 
 urlpatterns = [
     path('auth/register/', RegisterAPIView.as_view(), name='user-register'),
+    path('auth/login/', LoginAPIView.as_view(), name='user-login'),
     path('auth/verify-email/', VerifyEmailAPIView.as_view(), name='verify-email'),
 
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
--- project/apps/users/views.py
+++ project/apps/users/views.py
@@ -3,10 +3,12 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from apps.users.dtos.auth_response import UserAuthenticationService
 from apps.users.dtos.verification_response import VerificationResponse
-from apps.users.serializers import UserRegistrationSerializer, VerifyEmailSerializer
+from apps.users.serializers import UserRegistrationSerializer, VerifyEmailSerializer, UserLoginSerializer
 from apps.users.services.email_verification import VerificationService
 from apps.users.services.registration import register_user, RegisterUserCommand
+from apps.users.services.tokens import make_tokens
 
 
 # Create your views here.
@@ -40,5 +42,31 @@ class VerifyEmailAPIView(APIView):
 
         return Response(response_data.to_dict(), status=status.HTTP_200_OK)
 
+class LoginAPIView(APIView):
+    permission_classes = (AllowAny,)
 
+    def post(self, request):
+        serializer = UserLoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        result = UserAuthenticationService.authenticate_user(
+            email = serializer.validated_data['email'],
+            password = serializer.validated_data['password']
+        )
+
+        if not result.is_success():
+            return Response(
+                {"detail": result.error},
+                status=status.HTTP_401_UNAUTHORIZED
+            )
+        tokens = make_tokens(result.user)
+        return Response({
+            "message": "Login successful",
+            "user": {
+                "id": result.user.id,
+                "email": result.user.email,
+                "role": result.user.role,
+            },
+            "tokens": tokens
+        }, status=status.HTTP_200_OK)
 
